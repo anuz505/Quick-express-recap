6298e01189329cbb22d3eda2b22a7ba81069053f
anuj bhandari <anujb364@gmail.com>
2024-09-21 20:08:19 +0545

    learned about staicmethod and query methods for the user schema

--- mongo_and_mongoose/schema.js
+++ mongo_and_mongoose/schema.js
@@ -8,8 +8,19 @@ const userSchema = new mongoose.Schema({
     lowercase: true,
   },
   createdDate: { type: Date, default: Date.now },
-  bestFriend: mongoose.Schema.ObjectId,
+  bestFriend: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+  },
   hobbies: [String],
 });
+
+userSchema.statics.findByName = function (name) {
+  return this.find({ name: new RegExp(name, "i") });
+};
+
+userSchema.query.byName = function (name) {
+  return this.where({ name: RegExp(name, "i") });
+};
 
 module.exports = mongoose.model("User", userSchema);
--- mongo_and_mongoose/script.js
+++ mongo_and_mongoose/script.js
@@ -1,19 +1,49 @@
 const mongoose = require("mongoose");
 const User = require("./schema.js");
-run();
-async function run() {
+// run();
+thirdRun();
+
+async function thirdRun() {
   try {
     await mongoose.connect("mongodb://127.0.0.1:27017/testDB");
-    console.log("Database connection established");
-
-    console.log("Database connection");
-    const Anuj = await User.create({
-      name: "Anuj Bhandari",
-      age: 20,
-      email: "anuzb50@gmail.com",
-      hobbies: ["Learning japanese", "coding", "Games"],
-    });
+    const userQueryResult = await User.find().byName("Anuj");
+    const userStaticMethodResult = await User.findByName("Anuj");
+    console.log("userQueryResult", userQueryResult);
+    console.log("userStaticMethodResult", userStaticMethodResult);
   } catch (error) {
-    console.log("error message", error);
+    console.log(error.message);
   }
 }
+
+// async function secondRun() {
+//   try {
+//     await mongoose.connect("mongodb://127.0.0.1:27017/testDB");
+//     console.log("Database connection established");
+//     const user = await User.where("age").lte(20).populate("bestFriend");
+//     console.log(user);
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// }
+
+// async function run() {
+//   try {
+//     await mongoose.connect("mongodb://127.0.0.1:27017/testDB");
+//     console.log("Database connection established");
+//     const Anuj = await User.create({
+//       name: "Anuj Bhandari",
+//       age: 20,
+//       email: "anuzb50@gmail.com",
+//       hobbies: ["Learning japanese", "coding", "Games"],
+//     });
+//     Anuj.bestFriend = "66ec27444b713d4d1ec73bf8";
+//     await Anuj.save();
+
+//     console.log("User Saved");
+//   } catch (error) {
+//     console.log("error message", error.message);
+//   } finally {
+//     await mongoose.connection.close();
+//     console.log("Database connection is closed now");
+//   }
+// }
